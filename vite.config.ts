736f7b1be6d598c1e21fd608a74880ad23b2e901
Sanchir01 <emgushovs@mail.ru>
2025-03-20 00:06:08 +0300

    add compress

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { compression } from 'vite-plugin-compression2'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		compression({
+			algorithm: 'gzip',
+			threshold: 1024
+		})
+	],
 	resolve: {
 		alias: {
 			'~': '/src'
